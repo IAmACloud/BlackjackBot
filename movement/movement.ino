a685716931deab12cb315156206645b55dab6677
MKvartsen <kvartsen@chalmers.se>
2025-10-17 14:16:14 +0200

    Completed movements before merge with FSM

--- movement/movement.ino
+++ movement/movement.ino
@@ -1,7 +1,15 @@
-
 #include <Arduino.h>
 #include <Servo.h>
+#include <ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Header.h>
+#include <std_msgs/UInt16.h>
 
+//#include "MovementControl.h"
+//#include "MovementComplete.h"
+
+ros::NodeHandle hubert;
 
 //Servos
 Servo body;
@@ -12,17 +20,6 @@ Servo elbow;
 Servo gripper;
 Servo wrist;
 
-//Publisher
-std_msgs::bool complete;
-ros::Publisher movement_complete("movement_complete", complete);
-ros::Publisher camera_up("camera_up", complete);
-ros::Publisher camera_down("camera_down", complete);
-
-//Subscriber
-ros::Subscriber<std_msgs::string> sub("/deal_card", deal_card);
-ros::Subscriber<std_msgs::string> sub("/turn_direction", turn_direction);
-ros::Subscriber<std_msgs::string> sub("/celebrate", celebrate);
-ros::Subscriber<std_msgs::string> sub("/move_camera", move_camera);
 
 // Enum for servo indices
 enum ServoIndex {
@@ -36,17 +33,18 @@ enum ServoIndex {
   SERVO_COUNT = 7
 };
 
-//Init position of all servos
-const int servo_pins[SERVO_COUNT] = {3, 5, 6, 9, 10, 11, 8};
+//Init position of all servos {7, 5, 6, 9, 10, 11, 8};
+const int servo_pins[SERVO_COUNT] = {7, 2, 6, 9, 10, 11, 8};
 
 int curr_pos[SERVO_COUNT];
+const int pos_init[SERVO_COUNT] =  {1700, 1500, 1460, 2300, 900, 1000, 450};
 
 // to be updated
-const int bodyDodge = 550;
-const int bodyDeck = 1300;
-const int bodyP1 = 1700;
-const int bodyP2 = 2000;
-const int bodyHouse = 2200;
+const int bodyDeck = 1000;
+const int bodyDodge = 1250;
+const int bodyP1 = 1400;
+const int bodyP2 = 1700;
+const int bodyHouse = 2300;
 
 const int headUp = 2000;
 const int headDown = 1700;
@@ -55,7 +53,7 @@ const int shoulderCelebrate = 1500;
 
 const int elbowRaised = 2100;
 const int elbowDeck = 1750;
-const int elbowPlace = 1500;
+const int elbowPlace = 1400;
 
 const int gripperOpen = 1000;
 const int gripperClosed = 1500;
@@ -63,14 +61,13 @@ const int gripperClosed = 1500;
 const int wristNeutral = 450;
 const int wristPlace = 1850;
 
-
-const int pos_init[SERVO_COUNT] =  {1700, 1500, 2000, 2300, 900, 1000, 450};
-const int pos_dodge[SERVO_COUNT] = {900, 2000, 2000, 2300, 2100, 1000, 450};
-const int pos_deck[SERVO_COUNT] =  {2100, 2000, 2000, 2300, 2100, 1000, 450};
-const int pos_pick[SERVO_COUNT] =  {2100, 1700, 2000, 2300, 1750, 1000, 450};
-const int pos_place[SERVO_COUNT] = {1700, 2000, 2000, 2300, 1500, 1500, 1850};
-
 int count = 0;
+
+std_msgs::Bool complete;
+//Publisher
+ros::Publisher movement_complete("movement_complete", &complete);
+ros::Publisher camera_up("camera_up", &complete);
+ros::Publisher camera_down("camera_down", &complete);
 
 void actuate_servo(Servo &servo, int servo_index, const int new_pos) {
   int diff, steps, now, CurrPwm, NewPwm, delta = 6;
@@ -79,7 +76,6 @@ void actuate_servo(Servo &servo, int servo_index, const int new_pos) {
   now = curr_pos[servo_index];
   CurrPwm = now;
   NewPwm = new_pos;
-
   if (CurrPwm == NewPwm) return; // No movement needed
 
   // determine direction (+1 or -1)
@@ -94,15 +90,18 @@ void actuate_servo(Servo &servo, int servo_index, const int new_pos) {
   }
   curr_pos[servo_index] = now;
   delay(10);
+  hubert.spinOnce();
 }
 
-// Move to dodge pos
+// Used as a startup-sequence
 void actuate_dodge() {
   actuate_servo(headPan, HEAD_PAN, headUp);
   actuate_servo(body, BODY, bodyDodge);
   actuate_servo(elbow, ELBOW, elbowRaised);
+  actuate_servo(gripper, GRIPPER, gripperClosed);
 } 
 
+// Help functions for atomic robot behaviour
 void actuate_headUp() {
   actuate_servo(headPan, HEAD_PAN, headUp);
 }
@@ -185,100 +184,195 @@ void actuate_celebrateP2() {
 }
 
 // Celebrate House
-void actuate_celebrateP1() {
+void actuate_celebrateHouse() {
   actuate_servo(body, BODY, bodyHouse);
   actuate_servo(shoulder, SHOULDER, shoulderCelebrate);
+  actuate_servo(elbow, ELBOW, elbowDeck);
+  actuate_servo(elbow, ELBOW, elbowRaised);
   actuate_servo(elbow, ELBOW, elbowDeck);
   actuate_servo(elbow, ELBOW, elbowRaised);
   actuate_servo(shoulder, SHOULDER, pos_init[SHOULDER]);
 }
 
 //Functions for the messages we subscribe for:
-void deal_card(std_msgs::string deal_msg){
-  switch(deal_msg){
-    case "DEAL 0":
-      //deal to p1
-      movement_complete.publish(true);
+void start_sequence(const std_msgs::String& start_msg) {
+  const String& msg = start_msg.data;
+  if(msg == "START GAME"){
+    //Raise arm in safe way
+    actuate_dodge();
+    complete.data = true;
+    //complete.header = ros::Time::now();
+    movement_complete.publish(&complete);
+  }
+  else {
+    // bad string
+    complete.data = false;
+    //complete.header = ros::Time::now();
+    movement_complete.publish(&complete);
+  }
+}
+
+void deal_card(const std_msgs::String& deal_msg){
+  const String msg = deal_msg.data;
+  //get player (digit)
+  char last_char = msg.charAt(msg.length() - 1);
+  if (last_char < '0' || last_char > '9') return;
+  int player = last_char - '0';
+  switch(player){
+    case 0:
+      //deal to house
+      actuate_deck();
+      actuate_dealHouse();
+      complete.data = true;
+      //complete.header.stamp = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
     
-    case "DEAL 1":
-      //deal to p2
-      movement_complete.publish(true);
+    case 1:
+      actuate_deck();
+      actuate_dealP1();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
 
-    case "DEAL 2":
-      //deal to house
-      movement_complete.publish(true);
+    case 2:
+      actuate_deck();
+      actuate_dealP2();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
     
     default:
       //bad string
-      movement_complete.publish(false);
+      complete.data = false;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
   }
 }
 
-void turn_direction(std_msgs::string turn_msg){
-  switch(turn_msg){
-    case "TURN 0":
+void turn_direction(const std_msgs::String& turn_msg){
+  const String& msg = turn_msg.data;
+  //get player (digit)
+  char last_char = msg.charAt(msg.length() - 1);
+  if (last_char < '0' || last_char > '9') return;
+  int player = last_char - '0';
+  switch(player){
+    case 0:
+      //turn to House
+      actuate_turnHouse();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
+      break;
+    case 1:
       //turn to p1
-      movement_complete.publish(true);
+      actuate_turnP1();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
-    case "TURN 1";
-      //turn to p2
-      movement_complete.publish(true);
-      break;
-    case "TURN 2";
-      //turn to house
-      movement_complete.publish(true);
+    case 2:
+      //turn to P2
+      actuate_turnP2();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
     default:
       //bad message
-      movement_complete.publish(false);
+      complete.data = false;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
   }
 }
 
-void celebrate(std_msgs::string celeb_msg){  
-  switch(celeb_msg){
-    case "CELEBRATE 0":
+void celebrate(const std_msgs::String& celeb_msg){  
+  const String& msg = celeb_msg.data;
+  //get player (digit)
+  char last_char = msg.charAt(msg.length() - 1);
+  if (last_char < '0' || last_char > '9') return;
+  int player = last_char - '0';
+  switch(player){
+    case 0:
+      //celebrate House
+      actuate_celebrateHouse();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
+      break;
+    case 1:
       //celebrate p1
-      movement_complete.publish(true);
+      actuate_celebrateP1();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
+      
       break;
-    case "CELEBRATE 1";
-      movement_complete.publish(true);
-      //celebrate p2
-      break;
-    case "CELEBRATE 2";
-      //celebrate house
-      movement_complete.publish(true);
+    case 2:
+      //celebrate P2
+      actuate_celebrateP2();
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
     default:
       //bad message
-      movement_complete.publish(false);
+      complete.data = false;
+      //complete.header = ros::Time::now();
+      movement_complete.publish(&complete);
       break;
   }
 }
 
-void move_camera(std_msgs::string cam_msg){
-  switch(cam_msg){
-    case "CAMERA UP":
+void move_camera(const std_msgs::String& cam_msg){
+  const String& msg = cam_msg.data;
+  if(msg == "CAMERA UP"){
       //move camera up
       actuate_headUp();
-      camera_up.publish(true);
-      break;
-    case "CAMERA DOWN";
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      camera_up.publish(&complete);
+    }
+    else if(msg == "CAMERA DOWN"){
       //move camera down
       actuate_headDown();
-      camera_down.publish(true);
-      break;
-    default:
-      break;
-  }
+      complete.data = true;
+      //complete.header = ros::Time::now();
+      camera_down.publish(&complete);
+    }
 }
 
+//For testing ROS-connection
+void messageCb(const std_msgs::UInt16& msg){
+  hubert.loginfo("GOT MESSAGE");
+  actuate_servo(body, BODY, msg.data);
+}
+
+//Subscriber
+ros::Subscriber<std_msgs::String> subStart("start", &start_sequence);
+ros::Subscriber<std_msgs::String> subDeal("deal_card", &deal_card);
+ros::Subscriber<std_msgs::String> subTurn("turn_direction", &turn_direction);
+ros::Subscriber<std_msgs::String> subCeleb("celebrate", &celebrate);
+ros::Subscriber<std_msgs::String> subMoveCam("move_camera", &move_camera);
+ros::Subscriber<std_msgs::UInt16> subEcho("chatter", messageCb);
 void setup() {
   Serial.begin(57600); // Starts the serial communication
+  //hubert.getHardware()->setPort(&Serial);
+  hubert.getHardware()->setPort(&SerialUSB);
+  hubert.initNode();
+  hubert.subscribe(subStart);
+  hubert.subscribe(subDeal);
+  hubert.subscribe(subTurn);
+  hubert.subscribe(subCeleb);
+  hubert.subscribe(subMoveCam);
+
+  hubert.advertise(movement_complete);
+  //For testing ROS-connection
+  //hubert.subscribe(subEcho);
 
 	//Attach each joint servo
 	//and write each init position
@@ -303,8 +397,6 @@ void setup() {
   wrist.attach(servo_pins[6]);
   wrist.writeMicroseconds(pos_init[6]);
 
-  Serial.println("Hubert test program started.");
-
   // We keep track of the current poses in the curr_pos array
   byte i;
   for (i=0; i<(sizeof(pos_init)/sizeof(int)); i++){
@@ -315,7 +407,10 @@ void setup() {
 }
 
 void loop() {
+  if(count == 0) {
+    actuate_dodge();
+    count++;
+  }
   hubert.spinOnce();
-  
-
+  delay(1);
 }
